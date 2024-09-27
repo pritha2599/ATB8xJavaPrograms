2fb192c245c5aeaa13a9885216397b9442bacfd8
Hello world <biswaspritha9674@gmail.com>
2024-09-27 23:26:42 +0530

    Add this file to git

--- src/Sept/exe_04092024/Lab001.java
+++ src/Sept/exe_04092024/Lab001.java
@@ -1,4 +1,11 @@
 package Sept.exe_04092024;
 
 public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Performing some calculations:");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+    }
 }
--- src/Sept/exe_04092024/Lab002.java
+++ src/Sept/exe_04092024/Lab002.java
@@ -1,4 +1,7 @@
 package Sept.exe_04092024;
 
 public class Lab002 {
+    public static void main(String[] args){
+        System.out.println("Hi Pritha");
+    }
 }
